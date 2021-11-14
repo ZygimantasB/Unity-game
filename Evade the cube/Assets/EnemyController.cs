13b899fda60e2a8af40743307647aa47fc2b1666
Zygimantas <brinkis.zygimantas@gmail.com>
2021-11-14 15:40:36 +0200

    Add project

---
+++ Evade the cube/Assets/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EnemyType
+{
+    Catcher,
+    Evader 
+}
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public EnemyType enemyType;
+
+    private readonly float m_TresholdPositionZ = -25.5f;
+    private PlayerController m_PlayerC;
+
+    private void Start()
+    {
+        m_PlayerC = GameObject
+            .Find("Player")
+            .GetComponent<PlayerController>();
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            transform.position.z - speed * Time.deltaTime);
+
+        // atskaičiuoti atstuvą tarp prie6o ir veik4jo pozicijos
+
+        if (Vector3.Distance(m_PlayerC.transform.position, transform.position) < 1.0f)
+        {
+            m_PlayerC.ReceivDamage();
+            Destroy(gameObject);
+
+        }
+
+        if (transform.position.z <= m_TresholdPositionZ)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Evade the cube/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Transform leftwall;
+    public Transform rightwall;
+    private Stats m_Stats;
+    public HUDManager hudMananger;
+
+    private void Awake()
+    {
+        m_Stats = GetComponent<Stats>();
+        hudMananger.UpdateHealthText(m_Stats.health); 
+    }
+
+    // if FPS is 150 then update is callrd 150 and postion is changed by 150
+    private void Update() // Update  funckijoje mes įvesime Player judėjimus
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalPosition = transform.position.x + horizontalInput * speed * Time.deltaTime;
+
+        float playerSize = transform.localScale.x / 2;
+
+        if (horizontalPosition - playerSize <= leftwall.position.x + leftwall.localScale.x / 2) // Daliname i6 dviejų, nes mano veikėjo centras yra ne nuo krašto, o nuo vidurio. 
+        {
+            return;
+        }
+        if (horizontalPosition + playerSize >= rightwall.position.x - rightwall.localScale.x / 2)
+        {
+            return;
+        }
+
+
+        transform.position = new Vector3(
+            horizontalPosition,
+            1, 
+            transform.position.z);
+    }
+
+    public void ReceivDamage()
+    {
+        m_Stats.UpdateHealth(10);
+        hudMananger.UpdateHealthText(m_Stats.health);
+    }
+}
+ 
